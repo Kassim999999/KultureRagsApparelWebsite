3cf2fc38f7fd0d2c39d588b8592e0e8dd1aa0e46
Kassim <kassimrooney@gmail.com>
2025-03-26 14:37:54 +0300

    Fetched products

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let baseUrl = "http://localhost:3000/products"
+
+document.addEventListener("DOMContentLoaded", () => {})
+
+let productsBar = document.getElementById('products-bar')
+
+function fetchProducts() {
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(products => products.forEach(product => {
+        
+        let span = document.createElement('span');
+        span.textContent = product.name;
+        span.addEventListener('click', () => displayProducts(product));
+        productsBar.appendChild('span');
+    }))
+
+    .catch(err => console.log(err)
+    )
+}
