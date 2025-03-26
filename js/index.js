f339fd8736c6bb409637a35b78365c9465da250d
Kassim <kassimrooney@gmail.com>
2025-03-26 20:48:12 +0300

    Added a function for diplaying products

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 let baseUrl = "http://localhost:3000/products"
 
+function displayProducts(products) {
+    
+    let product_catalog = document.getElementById('product-catalog')
 
+    products.forEach(product => {
+        let html = `<div class="single-product">
+            <h5>${product.title}</h5>
+            <img src="${product.image}" alt="${product.title}">
+            <p>${product.description}...</p>
+            <div>${product.category}</div>
+            <div>$ ${product.price}</div>
+            <div id= "buttons">
+            <button class= " one update">Update</button>
+            <button id="delete-button" class= "one delete" onclick= "deleteProduct(${product.id})">Delete</button>
+            </div>
+        </div>`
+
+
+        product_catalog.innerHTML += html
+    });
+}
